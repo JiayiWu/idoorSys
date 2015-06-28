0faea941dfc8a623ad7dfd6370a0152954aa6f89
ZhengzhongZhang <zhengzhongzhang@outlook.com>
2015-06-28 16:55:50 +0800

    除去多余文件

--- src/main/java/com/idoorSys/MainTest.java
+++ src/main/java/com/idoorSys/MainTest.java
@@ -8,6 +8,7 @@ import com.idoorSys.model.Permission;
 import com.idoorSys.model.PermissionUser;
 import com.idoorSys.model.Room;
 import com.idoorSys.service.PeriodicPermissionService;
+import com.idoorSys.service.PermissionService;
 import com.idoorSys.utils.LocalIpAddressService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -19,16 +20,16 @@ public class MainTest {
 
 	public static void main(String args[]) throws Exception {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		BaseDao dao = ((BaseDao) context.getBean("periodicPermissionDao"));
-		System.out.println(dao.getSessionFactory().getAllClassMetadata());
-		List<PermissionUser> all = (List<PermissionUser>) dao.getAll(PermissionUser.class);
-		System.out.println(all);
+//		BaseDao dao = ((BaseDao) context.getBean("periodicPermissionDao"));
+//		System.out.println(dao.getSessionFactory().getAllClassMetadata());
+//		List<PermissionUser> all = (List<PermissionUser>) dao.getAll(PermissionUser.class);
+//		System.out.println(all);
 
 
-//		PeriodicPermissionService service = ((PeriodicPermissionService) context.getBean("periodicPermissionService"));
-//		for (PeriodicPermission permission : ((List<PeriodicPermission>) service.getAll())) {
-//			System.out.println(permission.getPermissionUser().getCardNum());
-//		}
+		PermissionService service = ((PermissionService) context.getBean("permissionService"));
+		for (Permission permission : ((List<Permission>) service.getAll())) {
+			System.out.println(permission.getPermissionUser().getCardNum());
+		}
 	}
 
 }
--- src/main/java/com/idoorSys/model/TempPermmtion.java
+++
@@ -1,5 +0,0 @@
-package com.idoorSys.model;
-
-public class TempPermmtion {
-
-}
